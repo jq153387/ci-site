e099cd5f62f242d65c9de63d3c43745e207e59ab
Leon Cheng <jq153387@Leonde-MacBook-Pro.local>
2020-07-22 16:18:32 +0800

    update

--- application/views/admin/default/album/index.php
+++ application/views/admin/default/album/index.php
@@ -1,4 +1,4 @@
-<div class="row">
+<head><link href="/assets/admin/app/default/album/app/main.bfd7e650aa623ebb4f9f.css" rel="stylesheet"></head><div class="row">
     <div class="col-md-12">
         <div class="box">
             <div class="box-body">
@@ -6,4 +6,4 @@
             </div>
         </div>
     </div>
-</div><script type="text/javascript" src="/assets/admin/default/app/album/dist/runtime.index.bundle.js"></script><script type="text/javascript" src="/assets/admin/default/app/album/dist/main.bundle.js"></script>
+</div><script type="text/javascript" src="/assets/admin/app/default/album/app/runtime.2e5f12047ef24713070e.index.bundle.js"></script><script type="text/javascript" src="/assets/admin/app/default/album/app/main.bfd7e650aa623ebb4f9f.bundle.js"></script>
